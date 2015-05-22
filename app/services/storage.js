7a147bad5e7af3f5b27dff0c07a2d5929aee777d
Mike Richards <mrichards42@gmx.com>
2015-05-21 20:02:44 -0500

    Factor localStorage out of config service

--- app/services/storage.js
+++ app/services/storage.js
@@ -5,17 +5,22 @@
 
 (function () {
     angular.module('PlantsApp')
-        .factory('storage', StorageService);
+        .factory('storage', ['$q', StorageService]);
 
-    function StorageService() {
+    function StorageService($q) {
         var self = {
             _data: {},
-            get: function (key) {
-                return self._data[key] || JSON.parse(localStorage.getItem(key));
+            get: function (key, fallback) {
+                return self._data[key] || (self._data[key] = (JSON.parse(localStorage.getItem(key)) || fallback));
             },
             set: function (key, value) {
                 self._data[key] = value;
                 localStorage.setItem(key, JSON.stringify(value));
+            },
+            getPromise: function(key, fallback) {
+                return $q(function(resolve) {
+                    resolve(self.get(key, fallback));
+                });
             }
         };
         return self;
