53cb9e7d15bff315b4b3bbff9acc04cfeff8a3df
Mike Richards <mrichards42@gmx.com>
2015-05-31 21:22:06 -0500

    function PouchDB.upsertView() to add or update a view

--- app/services/pouch.js
+++ app/services/pouch.js
@@ -1,5 +1,5 @@
 /**
- * Add PouchDB.putIfNotExists() and PouchDB.createDesignDoc()
+ * PouchDB Utility functions
  */
 
 (function () {
@@ -26,7 +26,7 @@
          * @param [reduceFunction]
          * @returns {{_id: string, views: {}}}
          */
-        'createDesignDoc': function(name, mapFunction, reduceFunction) {
+        'createViewDoc': function(name, mapFunction, reduceFunction) {
             var ddoc = {
                 _id: '_design/' + name,
                 views: {}
@@ -35,6 +35,32 @@
             if (reduceFunction)
                 ddoc.views[name].reduce = reduceFunction.toString();
             return ddoc;
+        },
+        /**
+         * Insert or update a view design document
+         * Compares map/reduce functions so the design doc isn't updated if it is the same
+         * @param name
+         * @param mapFunction
+         * @param [reduceFunction]
+         * @returns {Promise} get if doc exists, put if not
+         */
+        'upsertView': function(name, mapFunction, reduceFunction) {
+            var db = this;
+            var doc = db.createViewDoc(name, mapFunction, reduceFunction);
+            return db.get(doc._id).then(function(result) {
+                // Compare map/reduce functions
+                if (result.views[name].map != doc.views[name].map ||
+                        result.views[name].reduce != doc.views[name].reduce) {
+                    // Put
+                    doc._rev = result._rev;
+                    return db.put(doc);
+                }
+                return result;
+            }).catch(function (err) {
+                if (err.status === 404)
+                    return db.put(doc); // Doc does not exist; insert
+                throw err;
+            });
         }
     });
 })();
