512a4b6164c4faa7969c6ffe30c7c0bb51942e02
Mike Richards <mrichards42@gmx.com>
2015-05-31 21:24:39 -0500

    Remove duplicates in plantList.getPlants

--- app/components/plant/plantListService.js
+++ app/components/plant/plantListService.js
@@ -55,26 +55,46 @@
 
             /**
              * Get plants from a stored plant list
-             * @param {String} [name=ALL_PLANTS]
+             * @param {String} [id=ALL_PLANTS]
              * @param [options={include_docs:true}]
              * @returns {Promise} array of plants
              */
-            getPlants: function(name, options) {
+            getPlants: function(id, options) {
                 options = options || {};
-                options.include_docs = options.include_docs === undefined ? true : options.include_docs;
+                options.include_docs = options.include_docs == undefined ? true : options.include_docs;
                 var docs;
-                if (name === self.ALL_PLANTS || name === undefined) {
+                if (id == self.ALL_PLANTS || id === undefined) {
                     options.startkey = options.startkey === undefined ? 'A' : options.startkey;
                     options.endkey = options.endkey === undefined ? 'ZZZZ' : options.endkey;
+                    console.log('getPlants: allDocs', options);
                     docs = db.allDocs(options);
                 }
                 else {
-                    options.key = name;
+                    options.startkey = id;
+                    options.endkey = id + '\uffff';
                     options.reduce = options.reduce === undefined ? false : options.reduce;
+                    console.log('getPlants: query', options);
                     docs = db.query('list_items', options);
                 }
                 return docs.then(function(result) {
-                    return result.rows.map(function (row) { return row.doc; });
+                    var plants = [];
+                    var idSet = {};
+                    for (var i=0; i < result.rows.length; ++i) {
+                        var doc = result.rows[i].doc;
+                        var id = result.rows[i].value._id || result.rows[i].id;
+                        // Remove duplicates
+                        if (idSet[id])
+                            continue;
+                        idSet[id] = true;
+                        if (doc) {
+                            plants.push(doc);
+                        }
+                        else if (id != '2PLANT') {
+                            plants.push({_id: id, code:id, scientific:id});
+                            console.log('no plant', result.rows[i]);
+                        }
+                    }
+                    return plants;
                 });
             }
         };
