bac80629f94dd83b552bd74f94233e28c4ef6d20
Mike Richards <mrichards42@gmx.com>
2015-06-15 20:17:31 -0500

    Login/out of database from config

--- app/components/config/config.js
+++ app/components/config/config.js
@@ -1,19 +1,31 @@
 (function() {
     angular.module('PlantsApp')
-        .controller('ConfigCtrl', ['$scope', 'config', ConfigCtrl]);
+        .controller('ConfigCtrl', ['$scope', '$ionicPopup', 'config', ConfigCtrl]);
 
-    function ConfigCtrl($scope, config) {
+    function ConfigCtrl($scope, $ionicPopup, config) {
         var cfg = config.load();
+        var remoteDb = new PouchDB("https://" + cfg.username + ".cloudant.com/plants", {skipSetup: true});
         $scope.config = cfg;
         $scope.login = function() {
-            cfg.isLoggedIn = true;
-            cfg.save();
+            // Open db and try to login
+            remoteDb.login(cfg.username, cfg.password).then(function(result) {
+                cfg.isLoggedIn = true;
+                console.log(result);
+                cfg.save();
+            }).catch(function (err) {
+                console.log(err);
+                var alertPopup = $ionicPopup.alert({
+                    title: 'Could not log in',
+                    template: 'Check username and password'
+                });
+            });
         };
 
         $scope.logout = function() {
             cfg.password = "";
             if (! cfg.saveUsername)
                 cfg.username = "";
+            remoteDb.logout().then(console.log.bind(console));
             cfg.isLoggedIn = false;
             cfg.save();
         };
