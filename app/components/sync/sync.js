4170c76f9b7bbc106a591a44716f0ccc8134885b
Mike Richards <mrichards42@gmx.com>
2015-06-27 17:24:18 -0500

    Sync button checks that user is logged in

--- app/components/sync/sync.js
+++ app/components/sync/sync.js
@@ -1,11 +1,24 @@
 (function() {
     angular.module('PlantsApp')
-        .controller('SyncCtrl', ['$scope', 'toast', 'plantList', 'pouchReplicate', SyncCtrl]);
+        .controller('SyncCtrl', ['$scope', '$state', '$ionicPopup', 'toast', 'plantList', 'pouchReplicate', SyncCtrl]);
 
-    function SyncCtrl($scope, toast, plantList, pouchReplicate) {
+    function SyncCtrl($scope, $state, $ionicPopup, toast, plantList, pouchReplicate) {
         $scope.isSyncing = false;
 
-        $scope.sync = plantList.sync;
+        $scope.sync = function() {
+            plantList.sync().catch(function(err) {
+                if (err.name !== 'authentication_error')
+                    throw err;
+                // Ask user to login
+                $ionicPopup.confirm({
+                    title: 'Sync error',
+                    template: 'Please login to sync plants database',
+                    okText: 'Login'
+                }).then(function() {
+                    $state.go("config");
+                });
+            });
+        };
 
         pouchReplicate.on('start', function() {
             $scope.isSyncing = true;
