e53be97a0dbdad95c60c1b078923212be3be755d
Mike Richards <mrichards42@gmx.com>
2015-06-27 17:00:03 -0500

    Rewrite ConfigCtrl using new pouch and plantList functions

--- app/components/config/config.js
+++ app/components/config/config.js
@@ -1,52 +1,26 @@
 (function() {
     angular.module('PlantsApp')
-        .controller('ConfigCtrl', ['$scope', '$ionicPopup', 'config', ConfigCtrl]);
+        .controller('ConfigCtrl', ['$scope', '$ionicPopup', 'config', 'pouchDB', 'plantList', ConfigCtrl]);
 
-    function ConfigCtrl($scope, $ionicPopup, config) {
+    function ConfigCtrl($scope, $ionicPopup, config, pouchDB, plantList) {
         var cfg = config.load();
 
-        function remoteDb() {
-            return new PouchDB("https://" + cfg.database + ".cloudant.com/plants", {skipSetup: true});
-        }
-
-        /**
-         * Login to the DB with a token (aka UUID login)
-         * If no token exists, login with real credentials and create a token
-         */
-        function login() {
-            var db = remoteDb();
-            // Login with a token and save token to cfg
-            function tokenLogin(token) {
-                token = token || cfg.token || ['', ''];
-                return db.login(token[0], token[1]).then(function (result) {
-                    cfg.token = token;
-                    console.log('successfully logged in with token', token);
-                    return result;
-                });
-            }
-            return tokenLogin().catch(function(err) {
-                if (err.status !== 403 && err.status !== 400)
-                    throw err;
-                // Login with real credentials
-                return db.login(cfg.username, cfg.password).then(function(result) {
-                    console.log(result);
-                    // Create a dummy login to act as a token
-                    var token = [PouchDB.utils.uuid(), PouchDB.utils.uuid()];
-                    return db.signup(token[0], token[1]).then(function() {
-                        return tokenLogin(token);
-                    });
-                });
-            });
+        function getRemoteUrl() {
+            return "https://" + cfg.database + ".cloudant.com/plants";
         }
 
         $scope.config = cfg;
         $scope.login = function() {
             cfg.database = cfg.username;
-            login().then(function() {
-                // Don't save plaintext password
+            pouchDB.openRemote(getRemoteUrl()).then(function(db) {
+                // Start update
+                plantList.sync(db).then(console.log.bind(console)).catch(console.log.bind(console));
+                // Don't save credentials
+                if (! cfg.saveUsername)
+                    cfg.username = '';
                 cfg.password = '';
-                cfg.save();
                 cfg.isLoggedIn = true;
+                cfg.save();
                 $scope.$apply();
             }).catch(function (err) {
                 console.log(err);
@@ -58,12 +32,9 @@
         };
 
         $scope.logout = function() {
-            cfg.password = "";
-            if (! cfg.saveUsername)
-                cfg.username = "";
-            remoteDb().logout().then(console.log.bind(console));
-            cfg.save();
+            pouchDB.openRemote(getRemoteUrl(), {skipSetup:true}).logout();
             cfg.isLoggedIn = false;
+            cfg.save();
         };
     }
 })();
