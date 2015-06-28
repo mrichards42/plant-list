a3ce8f782d3074ee1c4068a58f62a5f74f35738a
Mike Richards <mrichards42@gmx.com>
2015-06-27 22:01:03 -0500

    User pouchdb-upsert * Remove old implementation and unused related functions

--- app/services/pouch.js
+++ app/services/pouch.js
@@ -8,20 +8,6 @@ angular.module('pouchdb').run(['$q', 'pouchDB', function($q, pouchDB) {
     // Utility functions
     PouchDB.plugin({
         /**
-         * Put a document if it does not exist
-         * @param doc
-         * @param [options]
-         * @returns {Promise} doc
-         */
-        'putIfNotExists': function(doc, options) {
-            return this.put(doc, options).catch(function(err) {
-                    if (err.status != 409)
-                        throw err;
-                    return doc;
-                }
-            )
-        },
-        /**
          * Create a design document for a view
          * The view and design doc share the name so queries can be made with just the name
          * @param name
@@ -38,44 +24,6 @@ angular.module('pouchdb').run(['$q', 'pouchDB', function($q, pouchDB) {
             if (reduceFunction)
                 ddoc.views[name].reduce = reduceFunction.toString();
             return ddoc;
-        },
-        /**
-         * Insert or update a doc
-         * @param doc
-         * @param [equalsFunction=angular.equals] Function to determine if two docs are equal
-         * @returns {Promise} get if doc exists, put if not
-         */
-        'upsert': function(doc, equalsFunction) {
-            var db = this;
-            equalsFunction = equalsFunction || angular.equals;
-            return db.get(doc._id).then(function(result) {
-                // Update _rev
-                doc._rev = result.rev;
-                if (! equalsFunction(doc, result))
-                    return db.put(doc);
-                else
-                    return result;
-            }).catch(function (err) {
-                if (err.status === 404)
-                    return db.put(doc); // Doc does not exist; insert
-                throw err;
-            });
-        },
-        /**
-         * Insert or update a view design document
-         * Compares map/reduce functions so the design doc isn't updated if it is the same
-         * @param name
-         * @param mapFunction
-         * @param [reduceFunction]
-         * @returns {Promise} get if doc exists, put if not
-         */
-        'upsertView': function(name, mapFunction, reduceFunction) {
-            var db = this;
-            var doc = db.createViewDoc(name, mapFunction, reduceFunction);
-            return db.upsert(doc, function(a, b) {
-                return a.views[name].map === b.views[name].map &&
-                    a.views[name].reduce === b.views[name].reduce;
-            });
         },
         /**
          * Open a new PouchDB on the same server
